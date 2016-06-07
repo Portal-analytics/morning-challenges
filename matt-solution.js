88f38f8b6d0f6f9a133b8cddf999029c156039e2
Matt McGuiness <matt.mcguiness@portalanalytics.us>
2016-06-07 08:31:34 -0400

    Solution-1-Edits
    Made the suggested edits.

--- matt-solution.js
+++ matt-solution.js
@@ -1,11 +1,13 @@
-var sum = 0;
-function oddNumbers() {
-	for (var i = 0; i < 1000; i++) {
-		if(i % 3 ===0 || i % 5 === 0) {
-			sum += i;
-		}
-	}
-	return sum;
+function sumOfThreeAndFive() {
+    var sumToOneThousand = 0;
+    for (var i = 0; i < 1000; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sumToOneThousand += i;
+        }
+    }
+    return sumToOneThousand;
+
+
 }
 
-console.log(oddNumbers());
+console.log(sumOfThreeAndFive());
