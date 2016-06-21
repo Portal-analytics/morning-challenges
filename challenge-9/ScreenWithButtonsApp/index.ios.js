7b4dad3d1f6aee92a83cb85c15febe3925e550e8
mattmcguin <matt.mcguiness@portalanalytics.us>
2016-06-21 10:31:43 -0400

    Day 9
    Couldnt quite get it to work and I ran out of time

---
+++ challenge-9/ScreenWithButtonsApp/index.ios.js
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Button from 'react-native-button';
+
+class ScreenWithButtonsApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Hello From Main
+        </Text>
+        <Button
+          Go to Home!
+        </Button>
+        <Button>
+          Show Modal
+        </Button>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ScreenWithButtonsApp', () => ScreenWithButtonsApp);
